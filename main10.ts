c2350af28206de68eed715e73290cd17b07cd489
kangheemung <kurotamashi30@gmail.com>
2025-10-12 17:32:16 +0900

    add

--- main10.ts
+++ main10.ts
@@ -1,27 +1,28 @@
-interface Product{
-    img:string;
-    name:string;
-    description:string
-    // discountPercent?:number
+interface Product {
+    img: string;
+    name: string;
+    description: string;
 }
-//エラーを起こさないため分ける
-interface salseProducts extends Product{
-    discountPercent:number
-}//＜＝このデータは必ずあるデータだ。！！
-type ApiResponse={
-    product:Product[],
-    salseProducts:Product[],
+
+interface SalesProduct extends Product {
+    discountPercent: number;
 }
-const apiResponse:ApiResponse = {
-    products:[
-            {img:"pants.png",name:"ズボン",description:"newpants"},
-            {img:"jacket.png",name:"ジャケット",description:"jacket"}
-        ],
-    salseProducts:[
-        {img:"skirts.png",name:"スカート",description:"skirts",discountPercent:20},
-        {img:"shirts.png",name:"シャーヅ",description:"shirts",discountPercent:10}
+
+type ApiResponse = {
+    products: Product[];
+    salesProducts: SalesProduct[];
+}
+
+const apiResponse: ApiResponse = {
+    products: [
+        { img: "pants.png", name: "ズボン", description: "newpants" },
+        { img: "jacket.png", name: "ジャケット", description: "jacket" }
+    ],
+    salesProducts: [
+        { img: "skirts.png", name: "スカート", description: "skirts", discountPercent: 20 },
+        { img: "shirts.png", name: "シャーヅ", description: "shirts", discountPercent: 10 }
     ]
 }
-apiResponse.map(item=?item.discountPercent)
-// apiResponse.map(item=>item.discountPercent)
-//에러 간다. 이유타입이 여러개여서
+
+// Error Explanation
+// The map function is not available on ApiResponse directly because it contains multiple types. 
