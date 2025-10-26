914455bd7afe047777e0629c44953357bf6b8d7b
kangheemung <kurotamashi30@gmail.com>
2025-10-26 13:39:08 +0900

    test

--- main10.ts
+++ main10.ts
@@ -1,11 +1,17 @@
-interface,type
+//interface, type
 
-interface Student{
+interface IStudent{
     name:string;
     age?:number;
-    isStudent:boolean;
+    //isStudent:boolean;
 }
-let b:Student={name:"noona",age:34}
-let c:{name:string,age:number}={name:"noona",age:23}
-let d:{name:string|number|boolean,age?:number,isStudent:boolean}={name:"asd",age:12}
-let e:{name:string|number|boolean,age?:number,isStudent:boolean}={name:"sdf",age:12}
+type TStudent ={
+    name:string,
+    age?:number
+}
+
+let b:IStudent={name:"noona",age:34};
+let c:IStudent={name:"noona",age:23};
+let d:TStudent={name:"asd",age:12};
+let e:TStudent={name:"sdf"};
+let f_fixed: IStudent = { name: "Hyun", age: 25 };
