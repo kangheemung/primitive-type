f524ae7110510e3d48a524ee6aea0bdc1d6703bf
kangheemung <kurotamashi30@gmail.com>
2025-11-29 18:12:22 +0900

    test

--- main15.ts
+++ main15.ts
@@ -1,22 +1,23 @@
-//naver
-//void
-function infinte(): never{
-    while(true){
-
-        }
+// Corrected function return type declarations
+function infinite(): never {
+    while (true) {}
 }
-function throwError(): naver{
+
+function throwError(): never {
     throw new Error("エラー");
 }
 
-type AorB = { a: number, b?: never} | { a?: naver ,b:number}
-let c1 :AorB ={
-    a:2
+type AorB = { a: number, b?: never } | { a?: never, b: number };
 
-}
-let c2: AorB = {
-    b: 3
+let c1: AorB = {
+    a: 2,
 };
+
+let c2: AorB = {
+    b: 3,
+};
+
 let c: AorB = {
     a: 2,
-}
+    b: 3, // Assigning a value to 'b' property matching the defined type
+};
